58a11b838d712ce47a845055cf93b8d7e0103ad9
Rishit-21 <rishit@webtheuniverse.com>
2022-06-21 12:27:58 +0530

    half assignment done

---
+++ assignment.js
@@ -0,0 +1,35 @@
+var country="India";
+const continent ="ASIA"
+var population=134000000
+console.log(country)
+console.log(continent)
+console.log(population)
+const isIsland = 0
+
+var language="gujrati"
+console.log(Boolean(isIsland))
+console.log(language)
+country="usa"
+console.log(population/2)
+console.log(population+1)
+var finlandPop=60000000
+if(population>finlandPop){
+    console.log("yes")
+}
+else
+{
+    console.log("no")
+}
+var avgPop=3300000
+if(population<avgPop){
+    console.log("yes")
+}
+else{
+    console.log("no")
+}
+if(population>avgPop){
+    console.log("india's population is above average")
+}
+else{
+    console.log("india'population is" , population-avgPop,  "belopw avg")
+}
