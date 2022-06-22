11b7e61a9e0c6dc4062156402dede72df3ef56d4
Rishit-21 <rishit@webtheuniverse.com>
2022-06-22 14:37:01 +0530

    done coding challenge and its assignment

--- assignment.js
+++ assignment.js
@@ -1,18 +1,22 @@
+//lecture: (values and vaiables) ,(data type),(let const and var)
 var country="India";
 const continent ="ASIA"
 var population=134000000
 console.log(country)
 console.log(continent)
 console.log(population)
-const isIsland = 0
-
-var language="gujrati"
+isIsland = 0
+var language="Chinese"
 console.log(Boolean(isIsland))
 console.log(language)
-country="INDIA"
+country="INDIA";
+console.log(typeof(isIsland),typeof(population), typeof(country),typeof(language))
+
+//lecture basic operators
 console.log(population/2)
 console.log(population+1)
 var finlandPop=60000000
+
 if(population>finlandPop){
     console.log("yes")
 }
@@ -20,13 +24,11 @@ else
 {
     console.log("no")
 }
+//taking decision if/else statements
 var avgPop=3300000
-if(population<avgPop){
-    console.log("yes")
-}
-else{
-    console.log("no")
-}
+var isAvg=population<avgPop
+    console.log(isAvg)
+
 if(population>avgPop){
     console.log("india's population is above average")
 }
@@ -34,25 +36,62 @@ else{
     console.log("india'population is" , population-avgPop,  "belopw avg")
     
 }
+// type conversin and coercioin
 var operation=5 + 6 + '4' + 9 - 4 - 2;
 console.log(operation)
 
+//equality operators
 var numBorders=1;
-if(numBorders==1){
-    if(numBorders===1){
-        console.log(`${country} has ${numBorders} neighbour`)
-    }
-    else{
-        console.log(`${country} has only one  neighbour`)
-    }
+if(Number(numBorders) ===1){
+    console.log("only one neighbour");
+}
+else if(numBorders>1){
+    console.log("more than one neighbours");
 }
 else{
-    console.log(`${country} has 0 neighbour`)
+    console.log("your country has 0 neighbour");
 }
+//logical operators
 let name1 = "sarah"
-if(population<5000000 && language=='English' && isIsland!=0){
+if(population<5000000 && language=='English' && isIsland){
     console.log(`you should leave in ${country} :)`)
 }
 else{
     console.log(`${country} does not  meet your criteria :(`)
 }
+//string and template literals
+var description=`${country} is in  ${continent} and its ${population} people speak ${language}`
+console.log(description)
+
+//the switch statement 
+switch(language){
+    case 'Chinese':
+    case 'Mandarin':
+        console.log("Most number of native speaker!");
+        break;
+    case 'Spanish':
+        console.log("2nd place in number of native speakers");
+        break;
+    case 'English':
+        console.log("3rd place");
+        break;
+    case 'Hindi':
+        console.log("number  Four");
+        break;
+    case 'Arabic':
+        console.log("5th most spoken language");
+        break;
+    default:
+        console.log("Great langauge to");
+}
+// condition ternary operators  on hand 
+ const age =23;
+ age >=18 ? console.log("I like to dirnk wine 🍷"):
+ console.log("I like to dirnk water");
+ const drink = age >=18 ? 'wine 🍷' : 'water 🌊';
+ console.log(drink);
+  
+ console.log(`I like to drink ${age >=18 ? 'wine' : "water"}`)
+
+ //lecture the conditional(ternary) operator
+ console.log(`${country}'s ${ population>avgPop ?  'population is above avg':  'populaiton is below avg'}`)
--- codingChalleng1.js
+++ codingChalleng1.js
@@ -1,3 +1,4 @@
+//coding challenge 1
 var johnMass = 92;
 var jhonHeight=1.95;
 var markMass=78;
@@ -7,6 +8,8 @@ var johnBmi=johnMass/jhonHeight**2;
 var markBmi=markMass/(markheight*markheight);
 console.log(johnBmi);
 console.log(markBmi);
+
+//coding challenge 2
 var markHigherBmi= markBmi>johnBmi;
 console.log(markHigherBmi);
 
@@ -16,3 +19,38 @@ if(markHigherBmi){
 else{
     console.log(`john's BMI is higher by ${johnBmi-markBmi} markBMI: ${markBmi} johnBMI: ${johnBmi}`)
 }
+
+//coding challenge 3
+
+var ds1 = 97;
+var ds2 = 112;
+var ds3= 101;
+var avgD = Number((ds1+ds2+ds3)/3);
+console.log(avgD)
+
+var ks1 =109;
+var ks2 =95;
+var ks3=106;
+var avgK=Number((ks1+ks2+ks3)/3);
+console.log(avgK)
+
+if(avgD>=100 && avgK>=100){
+    if(avgD>avgK){
+        console.log(`team dolphins are winners with avg score of ${avgD} `);
+    }
+    else if(avgD<avgK){
+        console.log(`team koalas are winners  with avg score of ${avgK}`);
+    }
+    else{
+        console.log(`there is tie between both the teams as both have same avg score `);
+    }
+}
+else{
+    console.log('both team are loser ');
+}
+//coding challenge 4
+
+var bill=Number(430);
+var tip= bill>=50 && bill<=300 ? (bill*15)/100 : (bill*20)/100;
+console.log(` your bill is ${bill} your tip is ${tip}  so the total bill is ${bill+Number(tip)}`);
+
